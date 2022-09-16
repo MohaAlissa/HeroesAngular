ca810e87af49bd3cfaf1c0cb7e84ab29cf0043de
MohaAlissa <Muhammadissa41@gmail.com>
2022-09-16 22:05:21 +0200

    add notifications compomemt

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -3,15 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.scss']
+  styleUrls: ['./notifications.component.scss'],
 })
 export class NotificationsComponent implements OnInit {
-  postImages = ['/assets/img/Heroes/1.jpeg', '/assets/img/Heroes/2.jpeg', '/assets/img/Heroes/3.jpeg', '/assets/img/Heroes/4.jpeg'];
-  posts = ['Your Daily Runddown is online now',' Mr. Nice viewed your profil', 'A post from your friend Tornado is popular',' New heroe has just joined your Group'];
-  canFollow =true;
-  constructor() { }
+  postImages = [
+    '/assets/img/Heroes/7.png',
+    '/assets/img/Heroes/10.png',
+    '/assets/img/Heroes/11.png',
+    '/assets/img/Heroes/5.jpeg',
+    '/assets/img/Heroes/12.png',
+    '/assets/img/Heroes/13.png',
+  ];
+  posts = [
+    'You May Know Dr. Hulk, would you like to send him a Messege',
+    ' Bombasto viewed your profil',
+    'A post from your friend Celeritas is popular',
+    ' Volkano has just joined your Group',
+    ' Narco is Online now, would you like to write him ?',
+    ' Weedey has similar interestes like you, would you like to add him?',
+  ];
+  canFollow = true;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
